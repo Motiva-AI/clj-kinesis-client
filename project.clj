b39c086ec85ea84de5cbc423cbae26c2e394c3ec
Paul Lam <paul@quantisan.com>
2022-09-26 08:01:51 +0900

    bump version and update libraries

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-kinesis-client "1.0.0"
+(defproject clj-kinesis-client "1.0.1-SNAPSHOT"
   :description "Clojure AWS Kinesis client"
-  :url "https://github.com/adtile/clj-kinesis-client"
+  :url "https://github.com/Motiva-AI/clj-kinesis-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.amazonaws/amazon-kinesis-client "1.7.2" :exclusions [com.amazonaws/aws-java-sdk-dynamodb]]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.amazonaws/amazon-kinesis-client "1.14.8" :exclusions [com.amazonaws/aws-java-sdk-dynamodb]]]
   :profiles {:dev {:dependencies [[clj-containment-matchers "1.0.1"]]}}
   :signing {:gpg-key "webmaster@adtile.me"})
