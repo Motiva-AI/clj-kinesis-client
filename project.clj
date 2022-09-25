02e1958f5b3cd020cf2c41e704c8afabb0324ac5
Paul Lam <paul@quantisan.com>
2022-09-26 08:05:06 +0900

    cleanup project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/Motiva-AI/clj-kinesis-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.amazonaws/amazon-kinesis-client "1.14.8" :exclusions [com.amazonaws/aws-java-sdk-dynamodb]]]
-  :profiles {:dev {:dependencies [[clj-containment-matchers "1.0.1"]]}}
-  :signing {:gpg-key "webmaster@adtile.me"})
+
+  :dependencies [[com.amazonaws/amazon-kinesis-client "1.14.8" :exclusions [com.amazonaws/aws-java-sdk-dynamodb]]]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
+                                  [clj-containment-matchers "1.0.1"]]}})
