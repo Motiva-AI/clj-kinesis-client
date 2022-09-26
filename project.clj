cad803d1f02030eba9e0e64b11a94386cc740b51
Florian Ishida <florian@motiva.ai>
2022-09-26 14:43:38 +0900

    deploy to clojars

--- project.clj
+++ project.clj
@@ -7,5 +7,13 @@
   :dependencies [[com.amazonaws/amazon-kinesis-client "1.14.8" :exclusions [com.amazonaws/aws-java-sdk-dynamodb]]
                  [org.clojure/data.json "2.4.0"]]
 
+  :plugins [[day8/lein-git-inject "0.0.14"]]
+  :middleware [leiningen.git-inject/middleware]
+
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
-                                  [clj-containment-matchers "1.0.1"]]}})
+                                  [clj-containment-matchers "1.0.1"]]}}
+
+  :repositories [["releases" {:url           "https://clojars.org/repo"
+                              :username      "motiva-ai"
+                              :password      :env
+                              :sign-releases false}]])
